3605b3f2f260b3bdac1935cc15392eb562912c95
sonercand <s.ism.candan@gmail.com>
2022-05-06 15:49:06 +0000

    chores

--- capitalAssetPricingModel.py
+++ capitalAssetPricingModel.py
@@ -18,26 +18,26 @@ from datetime import datetime, timedelta
 import pandas_datareader as web
 
 
-marketTic = "^GSPC"
-end = datetime.today()
-# end = datetime(2021, 12, 30)
-days = timedelta(365)
-start = end - days
-# start = datetime(2021, 1, 1)
-start = str(start.strftime("%Y-%m-%d"))
-end = str(end.strftime("%Y-%m-%d"))
+def calculateCAPM(marketTic, stockTic):
+    end = datetime.today()
+    # end = datetime(2021, 12, 30)
+    days = timedelta(365)
+    start = end - days
+    # start = datetime(2021, 1, 1)
+    start = str(start.strftime("%Y-%m-%d"))
+    end = str(end.strftime("%Y-%m-%d"))
 
-dfMarket = web.get_data_yahoo(marketTic, start, end)
-dfMarket.dropna(inplace=True)
-expectedReturnMarket = getExpectedReturn(dfMarket, "Close")
-print(expectedReturnMarket)
+    dfMarket = web.get_data_yahoo(marketTic, start, end)
+    dfMarket.dropna(inplace=True)
+    expectedReturnMarket = getExpectedReturn(dfMarket, "Close")
+    riskFreeRate = getRiskFreeReturn(1) / 100
+    beta = calculateBeta(stockTic=stockTic, marketTic=marketTic, period=1)
+    expectedReturnTic = riskFreeRate + beta * (expectedReturnMarket - riskFreeRate)
+    return expectedReturnTic
 
-riskFreeRate = getRiskFreeReturn(1) / 100
-print(riskFreeRate)
 
-stockTic = "MSFT"
-beta = calculateBeta(stockTic=stockTic, marketTic=marketTic, period=1)
-print(beta)
-
-expectedReturnTic = riskFreeRate + beta * (expectedReturnMarket - riskFreeRate)
-print(expectedReturnTic * 100)
+if __name__ == "__main__":
+    stockTic = "AAPL"
+    marketTic = "^GSPC"
+    ret = calculateCAPM(marketTic, stockTic)
+    print(ret)
