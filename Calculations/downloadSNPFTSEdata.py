9e0e0d2d9e0ca67d949be3f5e8f8787fd8b0143d
sonercand <s.ism.candan@gmail.com>
2022-05-13 09:43:38 +0000

    add: download ftse snp data

---
+++ Calculations/downloadSNPFTSEdata.py
@@ -0,0 +1,15 @@
+import yfinance as yf
+from portfolioAnalytics import getData
+import pandas as pd
+
+fComp = pd.read_csv("./data/ftse100components.csv")
+sComp = pd.read_csv("./data/snp500components.csv")
+
+tickers = []
+tickers = list(fComp["symbol"]) + list(sComp["Symbol"])
+print(len(tickers))
+
+period = "5y"
+data = getData(tickers, period)
+data.to_csv("./data/snpFtseClose.csv")
+print(data.head())
