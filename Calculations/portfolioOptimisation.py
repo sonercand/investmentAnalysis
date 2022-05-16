7cba5ffdf8055741c3e9ae8c7d4e0bc89cc0f1a7
sonercand <s.ism.candan@gmail.com>
2022-05-16 15:32:39 +0000

    add: portfolio optimisation for a given risk rate

--- Calculations/portfolioOptimisation.py
+++ Calculations/portfolioOptimisation.py
@@ -20,9 +20,8 @@ cascaded loops for each coef. (might take too long to calculate)
 
 
 """
-from pickletools import optimize
-from matplotlib import ticker
-from calcExpectedReturn import getExpectedReturn
+
+
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -30,181 +29,199 @@ from dateutil.relativedelta import relativedelta
 from scipy.optimize import minimize
 
 
-def calculateLogReturn(data):
-    return np.log(data / data.shift(1))
+class OptimisePortfolio:
+    def __init__(
+        self,
+        data,
+        period,
+        risk,
+        useLogReturns=True,
+        workDaysinYear=252,
+        objectFunction="Sharpe",
+    ):
+        self.data = data
+        self.period = period
+        self.workDaysinYear = workDaysinYear
+        self.useLogReturns = useLogReturns
+        self.risk = risk
+        self.useRiskRange = True if isinstance(risk, list) else False
+        self.objectFunction = objectFunction
 
+    def calculateLogReturn(self, data: pd.DataFrame):
+        """return log returns"""
+        return np.log(data / data.shift(1))
 
-def processData(data, period=2, useLogReturns=True):
-    data.reset_index(inplace=True)
-    data["Date"] = pd.to_datetime(data.Date)
-    tickers = list(data.columns)
-    tickers.remove("index")
-    tickers.remove("Date")
-    for col in tickers:
-        percent_missing = data[col].isnull().sum() * 100 / len(data[col])
-        if percent_missing > 70:  # remove ticks that have few data points
-            tickers.remove(col)
-    currentDay = datetime.now()
-    lastYearToday = currentDay - relativedelta(years=period)
-    data = data[
-        (pd.to_datetime(data["Date"]) > lastYearToday)
-        & (pd.to_datetime(data["Date"]) < currentDay)
-    ]
-    if useLogReturns:
-        dr = calculateLogReturn(data[tickers])  # log returns daily
-        dr.dropna(inplace=True)
-    else:
-        dr = data[tickers].pct_change(1).dropna()  # daily returns
-    covMatrix = dr.cov()
-    return dr, tickers, covMatrix
+    def expectedAnnualReturns(self, dr: pd.DataFrame):
+        """
+        args:
+            dr: pandas dataframe: daily returns for tickers
+        returns:
+            expReturnsAnnual: pandas dataframe:annualised returns where tickers are the dataframe index
+        """
+        expReturnsDaily = dr.mean()
+        workDaysInYear = self.workDaysinYear
+        expReturnsAnnual = ((1 + expReturnsDaily) ** (workDaysInYear)) - 1
+        return expReturnsAnnual
 
+    def portfolioReturns(
+        self, weights: np.ndarray, expReturnsAnnual: pd.DataFrame
+    ) -> float:
+        return np.sum(expReturnsAnnual * weights)
 
-def expectedAnnualReturns(dr):
-    expReturnsDaily = dr.mean()
-    workDaysInYear = 252
-    expReturnsAnnual = ((1 + expReturnsDaily) ** (workDaysInYear)) - 1
-    return expReturnsAnnual
+    def portfolioRisk(self, weights: np.ndarray, covMatrix: pd.DataFrame) -> float:
+        varPort = np.dot(weights.T, np.dot(covMatrix, weights))
+        try:
+            stdPort = np.sqrt(varPort)
+            stdPortAnnual = stdPort * np.sqrt(252)
+            return stdPortAnnual
+        except Exception as e:
+            print(e, varPort, weights)
 
-
-def portfolioReturns(weights, expReturnsAnnual):
-    return np.sum(expReturnsAnnual * weights)
-
-
-def portfolioRisk(weights, covMatrix):
-    varPort = np.dot(weights.T, np.dot(covMatrix, weights))
-    try:
-        stdPort = np.sqrt(varPort)
-        stdPortAnnual = stdPort * np.sqrt(252)
-        return stdPortAnnual
-    except Exception as e:
-        print(e, varPort, weights)
-
-
-def sharpeRatio(weights, expReturnsAnnual, covMatrix):
-    return portfolioReturns(
-        weights=weights, expReturnsAnnual=expReturnsAnnual
-    ) / portfolioRisk(weights, covMatrix)
-
-
-def setRandomWeights(n):
-    w = np.random.random(n)
-    return w / np.sum(w)
-
-
-def randomSolve(expReturnsAnnual, covMatrix, tickers, n_iter=3000):
-    results = []
-    k = 0
-    while k <= n_iter:
-        k += 1
-        weights = setRandomWeights(n=len(tickers))
-        portfolioReturn = portfolioReturns(
+    def sharpeRatio(
+        self,
+        weights: np.ndarray,
+        expReturnsAnnual: pd.DataFrame,
+        covMatrix: pd.DataFrame,
+    ) -> float:
+        return self.portfolioReturns(
             weights=weights, expReturnsAnnual=expReturnsAnnual
-        )
-        portRisk = portfolioRisk(weights=weights, covMatrix=covMatrix)
-        res = {
-            "weights": weights,
-            "returns": portfolioReturn,
-            "risk": portRisk,
-            "sharpeRatio": portfolioReturn / portRisk,
+        ) / self.portfolioRisk(weights, covMatrix)
+
+    def setRandomWeights(self, n: int) -> np.ndarray:
+        """arg: n: int: number of tickers"""
+        w = np.random.random(n)
+        return w / np.sum(w)
+
+    def genRandomPortfolios(
+        self,
+        expReturnsAnnual: pd.DataFrame,
+        covMatrix: pd.DataFrame,
+        tickers: list,
+        n_iter=3000,
+    ) -> pd.DataFrame:
+        """generate random portfolios and their risk and return values"""
+        results = []
+        setRandomWeights = self.setRandomWeights
+        k = 0
+        while k <= n_iter:
+            k += 1
+            weights = setRandomWeights(n=len(tickers))
+            portfolioReturn = self.portfolioReturns(
+                weights=weights, expReturnsAnnual=expReturnsAnnual
+            )
+            portRisk = self.portfolioRisk(weights=weights, covMatrix=covMatrix)
+            res = {
+                "weights": weights,
+                "returns": portfolioReturn,
+                "risk": portRisk,
+                "sharpeRatio": portfolioReturn / portRisk,
+            }
+            results.append(res)
+        output = pd.DataFrame(results)
+        return output
+
+    def processData(self):
+        data = self.data
+        period = self.period
+        useLogReturns = self.useLogReturns
+        calculateLogReturn = self.calculateLogReturn
+        data.reset_index(inplace=True)
+        data["Date"] = pd.to_datetime(data.Date)
+        tickers = list(data.columns)
+        tickers.remove("index")
+        tickers.remove("Date")
+        for col in tickers:
+            percent_missing = data[col].isnull().sum() * 100 / len(data[col])
+            if percent_missing > 70:  # remove ticks that have few data points
+                tickers.remove(col)
+        currentDay = datetime.now()
+        lastYearToday = currentDay - relativedelta(years=period)
+        data = data[
+            (pd.to_datetime(data["Date"]) > lastYearToday)
+            & (pd.to_datetime(data["Date"]) < currentDay)
+        ]
+        if useLogReturns:
+            dr = calculateLogReturn(data[tickers])  # log returns daily
+            dr.dropna(inplace=True)
+        else:
+            dr = data[tickers].pct_change(1).dropna()  # daily returns
+        covMatrix = dr.cov()
+        return dr, tickers, covMatrix
+
+    def portfolioSharpeRatioObjFun(self, weights, expReturnsAnnual, covMatrix):
+        return -1 * self.sharpeRatio(weights, expReturnsAnnual, covMatrix)
+
+    def sumofWeightsConstrait(self):
+        return {
+            "type": "eq",
+            "fun": lambda x: np.sum(x) - 1,
         }
-        results.append(res)
-    output = pd.DataFrame(results)
-    return output
 
+    def portfolioRiskRangeLBConstrait(self, weights, covMatrix):
+        min_ = self.risk[0]
+        return self.portfolioRisk(weights, covMatrix) - min_
 
-def portfolioRiskConstraint(weights, covMatrix, risk):
-    calcRisk = portfolioRisk(weights, covMatrix)
-    return calcRisk - risk
+    def portfolioRiskRangeUBConstrait(self, weights, covMatrix):
+        max_ = self.risk[1]
+        return max_ - self.portfolioRisk(weights, covMatrix)
 
+    def portfolioRiskConstraint(self, weights, covMatrix, risk):
+        calcRisk = self.portfolioRisk(weights, covMatrix)
+        return calcRisk - risk
 
-def portfolioRiskConstraintRangeLB(weights, covMatrix, minRisk):
-    min_ = minRisk
-    calcRisk = portfolioRisk(weights, covMatrix)
-    return calcRisk - min_  # calcRisk>min_
-
-
-def portfolioRiskConstraintRangeUB(weights, covMatrix, maxRisk):
-    max_ = maxRisk
-    calcRisk = portfolioRisk(weights, covMatrix)
-    return max_ - calcRisk  # -calcRisk+max_
-
-
-def portfolioReturnsObjFun(weights, expReturnsAnnual):
-    return -1 * portfolioReturns(weights=weights, expReturnsAnnual=expReturnsAnnual)
-
-
-def portfolioSharpeRatioObjFun(weights, expReturnsAnnual, covMatrix):
-    return -1 * sharpeRatio(weights, expReturnsAnnual, covMatrix)
-
-
-# portfolio optimisation for a given risk range
-
-
-def maximisePortfolioReturnRange(covMatrix, riskRange, tickers, expectedAnnualReturns):
-    constraintA = {
-        "type": "eq",
-        "fun": lambda x: np.sum(x) - 1,
-    }  # sum of the weights = 1
-    constraintB = {
-        "type": "ineq",
-        "fun": portfolioRiskConstraintRangeLB,
-        "args": (covMatrix, riskRange[0]),
-    }
-    constraintC = {
-        "type": "ineq",
-        "fun": portfolioRiskConstraintRangeUB,
-        "args": (covMatrix, riskRange[1]),
-    }
-    bounds = tuple((0, 1) for _ in range(len(tickers)))
-    initialWeights = setRandomWeights(len(tickers))
-    result = minimize(
-        fun=portfolioReturnsObjFun,
-        x0=initialWeights,
-        args=expectedAnnualReturns,
-        method="SLSQP",
-        bounds=bounds,
-        constraints=[constraintA, constraintB, constraintC],
-    )
-    return result["x"]
-
-
-# portfolio optimisation for a given risk value
-def maximisePortfolioReturn(covMatrix, risk, tickers, expectedAnnualReturns):
-    constraintA = {
-        "type": "eq",
-        "fun": lambda x: np.sum(x) - 1,
-    }  # sum of the weights = 1
-    constraintB = {
-        "type": "eq",
-        "fun": portfolioRiskConstraint,
-        "args": (covMatrix, risk),
-    }
-    bounds = tuple((0, 1) for _ in range(len(tickers)))
-    initialWeights = setRandomWeights(len(tickers))
-    result = minimize(
-        fun=portfolioReturnsObjFun,
-        x0=initialWeights,
-        args=expectedAnnualReturns,
-        method="SLSQP",
-        bounds=bounds,
-        constraints=[constraintA, constraintB],
-    )
-    return result["x"].round(3)
+    def maximizePortfolioReturns(self, covMatrix, tickers, expectedAnnualReturns):
+        if self.objectFunction == "Sharpe":
+            objFun = self.portfolioSharpeRatioObjFun
+        elif self.objectFunction == "Returns":
+            objFun = self.portfolioReturns
+        else:
+            print("Object function should be either Sharpe or Returns")
+        constraits = []
+        constraits.append(self.sumofWeightsConstrait())
+        if self.useRiskRange:
+            constraits.append(
+                {
+                    "type": "ineq",
+                    "fun": self.portfolioRiskRangeLBConstrait,
+                    "args": (covMatrix, self.risk),
+                }
+            )
+            constraits.append(
+                {
+                    "type": "ineq",
+                    "fun": self.portfolioRiskRangeUBConstrait,
+                    "args": (covMatrix, self.risk),
+                }
+            )
+        else:
+            constraits.append(
+                {
+                    "type": "eq",
+                    "fun": self.portfolioRiskConstraint,
+                    "args": (covMatrix, self.risk),
+                }
+            )
+        bounds = tuple((0, 1) for _ in range(len(tickers)))
+        initialWeights = self.setRandomWeights(len(tickers))
+        result = minimize(
+            fun=objFun,
+            x0=initialWeights,
+            args=(expectedAnnualReturns, covMatrix),
+            method="SLSQP",
+            bounds=bounds,
+            constraints=constraits,
+        )
+        return result["x"].round(3)
 
 
 data = pd.read_csv("./data/snpFtseClose.csv")
-dr, tickers, covMatrix = processData(data=data, period=2, useLogReturns=True)
-print(dr.head())
+op = OptimisePortfolio(data=data, period=2, risk=0.2)
+dr, tickers, covMatrix = op.processData()
+expectedAnnualReturns = op.expectedAnnualReturns(dr)
 
-eAR = expectedAnnualReturns(dr)
-print(eAR.loc["AAPL"])
-eAR = expectedAnnualReturns(dr)
-dfR = randomSolve(eAR, covMatrix=covMatrix, tickers=tickers)
-print(dfR.head())
-risk = [0.01, 0.2]
-result = maximisePortfolioReturnRange(
-    covMatrix, risk, tickers, expectedAnnualReturns=eAR
-)
-print(result)
-print(portfolioRisk(result, covMatrix))
-print(portfolioReturns(weights=result, expReturnsAnnual=eAR))
+res = op.maximizePortfolioReturns(covMatrix, tickers, expectedAnnualReturns)
+print(res)
+pr = op.portfolioReturns(res, expectedAnnualReturns)
+print(pr)
+pRisk = op.portfolioRisk(res, covMatrix)
+print(pRisk)
