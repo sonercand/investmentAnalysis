07d538b1f50aabc6bdf6890c8b6a63d1cb6a02cc
sonercand <s.ism.candan@gmail.com>
2022-05-17 09:14:15 +0000

    init: tests

---
+++ tests/test_portfolioOptimisation.py
@@ -0,0 +1,40 @@
+from Calculations.portfolioOptimisation import OptimisePortfolio
+from datetime import datetime, timedelta
+import pandas as pd
+
+
+global stocks
+stocks = ["A", "B", "C"]
+global closeValues
+closeValues = {
+    "A": [100, 100, 100, 100],
+    "B": [100, 110, 120, 140],
+    "C": ["1", "2", "3", "4"],
+    "date": [
+        datetime(2022, 1, 1),
+        datetime(2022, 1, 2),
+        datetime(2022, 1, 3),
+        datetime(2022, 1, 4),
+    ],
+}
+global data
+data = pd.DataFrame(closeValues)
+
+
+def test_init():
+    op = OptimisePortfolio(
+        data,
+        1,
+        risk=[0.2],
+        useLogReturns=True,
+        workDaysinYear=252,
+        objectFunction="Sharpe",
+    )
+    print(data)
+    print(op.data)
+    pd.testing.assert_frame_equal(op.data, data)
+    assert op.useRiskRange == True
+
+
+def test_test():
+    assert 1 == 1
