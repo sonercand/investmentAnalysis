6af2afbb59cd391c735c1ba4974f333e342beca1
sonercand <s.ism.candan@gmail.com>
2022-06-02 14:40:56 +0000

    add: capm port optimisation

---
+++ Calculations/CAPMstocks.py
@@ -0,0 +1,119 @@
+from time import strftime
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+
+class CAPM:
+    def __init__(self, stocks, start_date, end_date):
+        stocks = set(stocks)
+        self.stocks = list(stocks)
+        self.start_date = start_date
+        self.end_date = end_date
+        self.UKMarketTicker = "^FTMC"
+        self.USMarketTIcker = "^GSPC"
+        self.data = self.downloadData()
+        self.data.columns = stocks
+        self.splitUKUS()
+        self.setMarketData()
+
+    def splitUKUS(self):
+        self.ukStocks = [stock for stock in self.stocks if stock.upper().endswith(".L")]
+        self.usStocks = [
+            stock for stock in self.stocks if not stock.upper().endswith(".L")
+        ]
+
+    def setMarketData(self):
+        usm = yf.download(self.USMarketTIcker, self.start_date, self.end_date)[
+            "Adj Close"
+        ]
+        self.usMarket = (usm).resample("M").last()
+        uk = yf.download(self.UKMarketTicker, self.start_date, self.end_date)[
+            "Adj Close"
+        ]
+        self.ukMarket = (uk).resample("M").last()
+
+    def downloadData(self):
+        data_ = []
+        j = 0
+        for stock in self.stocks:
+            try:
+                dfT = yf.download(stock, self.start_date, self.end_date)["Adj Close"]
+                if len(dfT) > 5:
+                    data_.append(dfT)
+                else:
+                    print("data frame has few rows")
+                    print(dfT)
+                    print(stock)
+            except Exception as e:
+                print(e)
+                print(stock)
+
+        df = pd.concat(data_, axis=1)
+        return df.resample("M").last()
+
+    def logReturns(self, data):
+        return (np.log(data / data.shift(1)))[1:]
+
+    def getRiskFreeRate(self):
+        """using 13 week treasure bill ^IRX"""
+        return yf.download("^IRX", self.start_date, self.end_date)["Adj Close"][-1]
+
+    def Beta(self, ticker):
+        if ticker in self.ukStocks:
+            covMatrix = np.cov(
+                self.logReturns(self.data[ticker]), self.logReturns(self.ukMarket)
+            )
+        elif ticker in self.usStocks:
+            covMatrix = np.cov(
+                self.logReturns(self.data[ticker]), self.logReturns(self.usMarket)
+            )
+        else:
+            return "Ticker not found"
+        return (
+            covMatrix[0, 1] / covMatrix[1, 1]
+        )  # note diagonals are the variance in the np cov matrix
+
+    def expectedReturnMarket(self, ticker):
+        if ticker in self.ukStocks:
+            return self.logReturns(self.ukMarket).mean()
+        if ticker in self.usStocks:
+            return self.logReturns(self.usMarket).mean()
+
+    def expectedReturnMonthly(self, ticker, numMonths=1):
+        riskFreeRate = self.getRiskFreeRate() / 100
+        return riskFreeRate + self.Beta(ticker) * (
+            self.expectedReturnMarket(ticker) * numMonths - riskFreeRate
+        )
+
+    def run(self):
+        expectedReturns = []
+        for stock in self.stocks:
+            try:
+                expectedReturns.append(
+                    {
+                        "ticker": stock,
+                        "expectedReturn": self.expectedReturnMonthly(stock, 12),
+                    }
+                )
+            except Exception as e:
+                print(stock)
+        return pd.DataFrame(expectedReturns)
+
+
+if __name__ == "__main__":
+    from datetime import datetime, date
+    from dateutil.relativedelta import relativedelta
+
+    today = date.today()
+    twoYearAgo = today - relativedelta(years=2)
+    today = today.strftime("%Y-%m-%d")
+    twoYearAgo = twoYearAgo.strftime("%Y-%m-%d")
+    print(today, twoYearAgo)
+    stocks = list(pd.read_csv("./data/snpFtseClose.csv").columns)
+    stocks.remove("Date")
+    print(len(stocks))
+    # stocks = ["mng.l", "AAPL", "MSFT", "PRU.L"]
+    capm = CAPM(stocks=stocks, start_date=twoYearAgo, end_date=today)
+    retDf = capm.run()
+    retDf.to_csv("./data/expectedReturns4monthlyCAPM.csv", index=False)
---
+++ Calculations/portfolioOptimisationCAPM.py
@@ -0,0 +1,215 @@
+from cmath import exp
+from re import L
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from scipy.optimize import minimize
+
+
+def processData(data, esgData, expectedQuarReturns, tickers, period=2):
+    tickers = [stock.upper() for stock in tickers]
+
+    currentDay = datetime.now()
+    lastYearToday = currentDay - relativedelta(years=period)
+    data["Date"] = pd.to_datetime(data["Date"])
+    data.set_index("Date", inplace=True)
+    data.sort_index(inplace=True)
+    data = data[
+        (pd.to_datetime(data.index) > lastYearToday)
+        & (pd.to_datetime(data.index) < currentDay)
+    ][tickers]
+
+    data = data.resample("M").last()
+    monthlyLogReturns = np.log(data / data.shift(1))[1:]
+    covMatrix = monthlyLogReturns.cov()
+    esgData = esgData[tickers]
+    expectedQuarReturns = expectedQuarReturns[expectedQuarReturns.ticker.isin(tickers)]
+    return data, monthlyLogReturns, covMatrix, esgData, expectedQuarReturns
+
+
+def portfolioReturn(data, expectedQuarReturns, weights):
+    k = 0
+    sum_ = 0
+    for ticker in data.columns:
+        ret = expectedQuarReturns[expectedQuarReturns.ticker == ticker][
+            "expectedReturn"
+        ].values[0]
+        sum_ += ret * weights[k]
+
+        k += 1
+    return sum_ * 3
+
+
+def portfolioRisk(covMatrix, weights):
+    varPort = np.dot(weights.T, np.dot(covMatrix, weights))
+    try:
+        stdPort = np.sqrt(varPort)
+        stdPortAnnual = stdPort * np.sqrt(12)
+        return stdPortAnnual
+    except Exception as e:
+        print(e, varPort, weights)
+    return None
+
+
+def portfolioESGscore(weights, esgData):
+    return np.dot(esgData, weights)[0]
+
+
+def sharpeRatio(data, weights, expectedQuarReturns, covMatrix):
+    return portfolioReturn(data, expectedQuarReturns, weights) / portfolioRisk(
+        covMatrix, weights
+    )
+
+
+def portfolioSharpeRatioObjFun(weights, data, expectedQuarReturns, covMatrix):
+    return -1 * sharpeRatio(
+        weights=weights,
+        data=data,
+        expectedQuarReturns=expectedQuarReturns,
+        covMatrix=covMatrix,
+    )
+
+
+def sumofWeightsConstrait():
+    return {
+        "type": "eq",
+        "fun": lambda x: np.sum(x) - 1,
+    }
+
+
+def portfolioESGConstrait(weights, esgScore, esgData):
+    """constrait: esgValue of Portfolio should be larger than the esgScore set by user"""
+    esgValue = portfolioESGscore(weights, esgData)
+    return esgValue - esgScore
+
+
+def portfolioRiskRangeLBConstrait(weights, covMatrix, risk):
+    min_ = risk[0]
+    return portfolioRisk(covMatrix, weights) - min_
+
+
+def portfolioRiskRangeUBConstrait(weights, covMatrix, risk):
+    max_ = risk[1]
+    return max_ - portfolioRisk(covMatrix, weights)
+
+
+def portfolioRiskConstraint(weights, covMatrix, risk):
+    calcRisk = portfolioRisk(covMatrix, weights)
+    return calcRisk - risk
+
+
+def maximizePortfolioReturns(
+    data,
+    covMatrix,
+    tickers,
+    expectedQuarReturns,
+    risk=None,
+    esgScore=None,
+    esgData=None,
+):
+    objFun = portfolioSharpeRatioObjFun
+    args = (data, expectedQuarReturns, covMatrix)
+    constraits = []
+    constraits.append(sumofWeightsConstrait())
+    if esgScore != None:
+        constraits.append(
+            {
+                "type": "ineq",
+                "fun": portfolioESGConstrait,
+                "args": (esgScore, esgData),
+            }
+        )
+    if isinstance(risk, list):
+        constraits.append(
+            {
+                "type": "ineq",
+                "fun": portfolioRiskRangeLBConstrait,
+                "args": (covMatrix, risk),
+            }
+        )
+        constraits.append(
+            {
+                "type": "ineq",
+                "fun": portfolioRiskRangeUBConstrait,
+                "args": (covMatrix, risk),
+            }
+        )
+    else:
+        constraits.append(
+            {
+                "type": "eq",
+                "fun": portfolioRiskConstraint,
+                "args": (covMatrix, risk),
+            }
+        )
+    bounds = tuple((0, 1) for _ in range(len(tickers)))
+    initialWeights = np.ones(len(tickers)) / len(
+        tickers
+    )  # self.setRandomWeights(len(tickers))
+    result = minimize(
+        fun=objFun,
+        x0=initialWeights,
+        args=args,
+        method="SLSQP",
+        bounds=bounds,
+        constraints=constraits,
+    )
+
+    return result["x"]
+
+
+def setRandomWeights(n: int) -> np.ndarray:
+    """arg: n: int: number of tickers"""
+    # w = np.random.randint(0, 1000, n)
+    w = np.random.random(n)
+    return w / np.sum(w)
+
+
+def genRandomPortfolios(
+    data, expectedQuarReturns, tickers, n_iter=10000
+) -> pd.DataFrame:
+    """generate random portfolios and their risk and return values"""
+    results = []
+    k = 0
+    while k <= n_iter:
+        k += 1
+        weights = setRandomWeights(n=len(tickers))
+        Pret = portfolioReturn(
+            data, weights=weights, expectedQuarReturns=expectedQuarReturns
+        )
+        Prisk = portfolioRisk(weights=weights, covMatrix=covMatrix)
+        res = {
+            "weights": weights,
+            "returns": Pret,
+            "risk": Prisk,
+            "sharpeRatio": Pret / Prisk,
+        }
+        results.append(res)
+    output = pd.DataFrame(results)
+    return output
+
+
+"""
+##example
+tickers = ["mng.l", "PRU.L", "AAPL", "MSFT"]
+data = pd.read_csv("./data/snpFtseClose.csv")
+expectedQuarReturns = pd.read_csv("./data/expectedReturns4monthlyCAPM.csv")
+esgData = pd.read_csv("./data/esgScores_aligned.csv")
+risk = [0, 1]
+data, monthlyLogReturns, covMatrix, esgData, expectedQuarReturns = processData(
+    data, esgData, expectedQuarReturns, tickers
+)
+
+
+optW = maximizePortfolioReturns(
+    data, covMatrix, tickers, expectedQuarReturns, risk
+).round(2)
+optRisk = portfolioRisk(covMatrix=covMatrix, weights=optW)
+optRet = portfolioReturn(data, expectedQuarReturns=expectedQuarReturns, weights=optW)
+
+
+randP = genRandomPortfolios(
+    data=data, expectedQuarReturns=expectedQuarReturns, tickers=tickers
+)
+"""
